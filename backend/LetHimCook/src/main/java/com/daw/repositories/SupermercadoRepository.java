72da77845edcc837304ace601e39ac31cde13b89
rauliesan <raullsdev@gmail.com>
2026-03-17 16:36:11 +0100

    Edición de Mappers y Repositorios

--- backend/LetHimCook/src/main/java/com/daw/repositories/SupermercadoRepository.java
+++ backend/LetHimCook/src/main/java/com/daw/repositories/SupermercadoRepository.java
@@ -1,10 +1,16 @@
 package com.daw.repositories;
 
-import com.daw.entities.Supermercado;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.UUID;
+
+import com.daw.entities.Supermercado;
 
 @Repository
 public interface SupermercadoRepository extends JpaRepository<Supermercado, UUID> {
+    Page<Supermercado> findByNombreContainingIgnoreCaseAndDireccionContainingIgnoreCase(
+            String nombre, String direccion, Pageable pageable);
 }
